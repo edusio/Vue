ec553555122187b6ecc522181179b04d5c9182b3
Eduardo López <edu.1990@hotmail.com>
2019-10-08 19:18:32 +0200

    create persona test component

---
+++ my-vueapp/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <persona></persona>
+</template>
+
+<script>
+  import persona from './components/Persona.vue';
+
+  export default {
+    
+    components: { persona },
+
+    data() {
+      return {};
+    }
+  }
+</script>
+
+<style>
+
+</style>
